8d3f3a7cec5b64a98dc563d9a5c7a1241a3352a3
Taylor Andrews <andrewstaylorj@gmail.com>
2014-11-19 15:26:40 -0700

    completed lab

--- facade.py
+++ facade.py
@@ -1,6 +1,5 @@
 import parts
 
-
 # The Computer class is a Facade class which will simplify the computer part classes.
 # You should implement this class
 # This class should be able to be called by the running part
@@ -10,14 +9,20 @@ class Computer:
         self.mem=parts.Memory()
         self.hd=parts.HardDisk()
     # implement the class below this line    
+    def startComputer(self):
+        self.cpu.check()
+        self.mem.load()
+        self.hd.mount()
 
+    def printCPUInfo(self):
+        self.cpu.getInfo()
 
+    def printMemInfo(self):
+        self.mem.getInfo()
 
-
-
-
-
-
+    def printHDInfo(self):
+        self.hd.getInfo()
+        
 # The running part
 # Don't modify the following code!
 com=Computer()
